71ff882d4f5b338f47c8f7954bd4098aa0cc1a2d
LEONE <leonwahungu@gmail.com>
2022-11-08 21:56:23 +0300

    Java to Kotlin Commit

--- app/src/main/java/com/sriyank/javatokotlindemo/activities/MainActivity.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/activities/MainActivity.kt
@@ -8,30 +8,64 @@ import com.sriyank.javatokotlindemo.R
 import android.content.SharedPreferences
 import android.content.Intent
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.sriyank.javatokotlindemo.activities.DisplayActivity
 import com.sriyank.javatokotlindemo.activities.MainActivity
 import com.sriyank.javatokotlindemo.app.Constants
+import kotlinx.android.synthetic.main.activity_main.*;
 
 class MainActivity : AppCompatActivity() {
-    private var etName: EditText? = null
-    private var etGithubRepoName: EditText? = null
-    private var etLanguage: EditText? = null
-    private var etGithubUser: EditText? = null
-    private var inputLayoutName: TextInputLayout? = null
-    private var inputLayoutRepoName: TextInputLayout? = null
-    private var inputLayoutGithubUser: TextInputLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
         setSupportActionBar(toolbar)
-        etName = findViewById(R.id.etName)
-        etGithubRepoName = findViewById(R.id.etRepoName)
-        etLanguage = findViewById(R.id.etLanguage)
-        etGithubUser = findViewById(R.id.etGithubUser)
-        inputLayoutName = findViewById(R.id.inputLayoutName)
-        inputLayoutRepoName = findViewById(R.id.inputLayoutRepoName)
-        inputLayoutGithubUser = findViewById(R.id.inputLayoutGithubUser)
+
+    }
+
+    /**Save app username in SharedPreferences*/
+    fun saveName(view: View) {
+        Toast.makeText(applicationContext,"Feature is currently unavailable",Toast.LENGTH_LONG).show()
+    }
+
+    /**Search repositories on github after passing data to DisplayActivity*/
+    fun listRepositories(view: View) {
+        if (isNotEmpty(etRepoName, inputLayoutRepoName)) {
+            val queryRepo = etRepoName.text.toString()
+            val repoLanguage = etLanguage.text.toString()
+
+            val intent = Intent(this, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
+            intent.putExtra(Constants.KEY_REPO_SEARCH, queryRepo)
+            intent.putExtra(Constants.KEY_LANGUAGE, repoLanguage)
+            startActivity(intent)
+        }
+    }
+
+
+    /**Search repositories of a particular github user after passing data to DisplayActivity*/
+    fun listUserRepositories(view: View) {
+        if (isNotEmpty(etGithubUser, inputLayoutGithubUser)) {
+            val githubUser = etGithubUser.text.toString()
+
+            val intent = Intent(this, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
+            intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
+            startActivity(intent)
+        }
+    }
+
+
+    private fun isNotEmpty(editText: EditText, textInputLayout: TextInputLayout): Boolean {
+        return if (editText.text.toString().isEmpty()) {
+            textInputLayout.error = "Cannot be blank!"
+            false
+        }
+        else {
+            textInputLayout.isErrorEnabled = false
+            true
+        }
     }
 }
