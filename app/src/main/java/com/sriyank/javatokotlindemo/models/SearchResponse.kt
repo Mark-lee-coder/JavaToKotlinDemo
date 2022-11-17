2041e5705ce06234bc5d07c75ed9e365ddd316ca
LEONE <leonwahungu@gmail.com>
2022-11-17 10:34:06 +0300

    Java to Kotlin Commit

--- app/src/main/java/com/sriyank/javatokotlindemo/models/SearchResponse.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/models/SearchResponse.kt
@@ -4,4 +4,7 @@ import com.google.gson.annotations.SerializedName
 import io.realm.RealmList
 import io.realm.RealmObject
 
-data class SearchResponse(@SerializedName("total_count") var totalCount : Int, var items: RealmList<Repository>?) : RealmObject()
+open class SearchResponse(
+    @SerializedName("total_count") var totalCount : Int = 0,
+    var items: RealmList<Repository>? = null
+) : RealmObject()
